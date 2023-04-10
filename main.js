cdeef7a4bfa84cea6293deb4de70209edaa8de4e
unknown <aux.asoriano@gmail.com>
2023-04-09 21:52:26 -0400

    sidsaV1

--- main.js
+++ main.js
@@ -1,14 +1,20 @@
 console.log("sorido0");
 
 const cita = document.querySelector("#este").addEventListener("click", () => {
-    console.log("sorido1");
-    Swal.fire({
-        icon: "info",
-        title: "Las citas online aun no estan disponibles",
-        text: "Puedes llamarnos por telefono o escribenos a whatsapp",
-        text: "809-593-0730, 809-593-7883 y 809-696-3854",
+    
+    
+    
+    window.open("https://docs.google.com/forms/d/e/1FAIpQLSeLH2m03-COkPx7QIPKwWXHnV0rRsryRmjW-R73j7hKddcQKg/viewform?usp=sf_link", "_blank");
+    
+    
+    // console.log("sorido1");
+    // Swal.fire({
+    //     icon: "info",
+    //     title: "Las citas online aun no estan disponibles",
+    //     text: "Puedes llamarnos por telefono o escribenos a whatsapp",
+    //     text: "809-593-0730, 809-593-7883 y 809-696-3854",
        
-    });
+    // });
 });
 //console.log(cita);
 
